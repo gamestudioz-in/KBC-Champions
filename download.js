2c6ba20e12976eb2303c88f073e17efe597916ad
gamestudioz-in <arealgmr@gmail.com>
2024-10-23 20:24:31 +0530

    Update download.js

--- download.js
+++ download.js
@@ -28,13 +28,13 @@ function showLink() {
         clickedButton.innerHTML = "<i style='color: white; font-size: 16px;' class='bx bxs-lock-open-alt'></i> Get Link";
     clickedButton.style.background = 'green';
         if (btnValue === 'mediafire') {
-            parentLink.href = 'https://www.mediafire.com/file/gvhqk5n4b53zx5a/KBC_Champions.zip/file';
+            parentLink.href = 'https://www.mediafire.com/file/84azyn3puzg9dj4/KBC_Champions%255B1.1%255D.apk/file';
         } 
         else if (btnValue === 'dropbox') {
-            parentLink.href = 'https://www.dropbox.com/scl/fi/um1mj9k6xocso63l3yvrv/KBC-Champions.zip?rlkey=bnbm7rmx1rca9kjh7swcmxw9o&st=yrqjo26h&dl=0';
+            parentLink.href = 'https://www.dropbox.com/scl/fi/b5ldf7cb3jat1aulplrcv/KBC-Champions-1.1.apk';
         } 
         else {
-            parentLink.href = 'https://www.drive.google.com/file/d/1fqWr7mNzI0Ypr0ja_1fTq7FYoU28p3Jp/view';
+            parentLink.href = 'https://www.mediafire.com/file/84azyn3puzg9dj4/KBC_Champions%255B1.1%255D.apk/file';
         }
     }, 5000);
 }
